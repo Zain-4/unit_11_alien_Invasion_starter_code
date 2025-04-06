65105bc88c61ef718361f3c1853c1006e57997e4
Zain-4 <zainsuddle1966@gmail.com>
2025-04-06 11:13:34 -0400

    Added events for ship moving left and right, built in boundaries.

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,9 +12,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_w,self.settings.screen_h)
             )
-        pygame.display.set_caption("self.settings.name")
+        pygame.display.set_caption(self.settings.name)
 
-        self.bg = pygame.image.load(self.settings.bg_filee)
+        self.bg = pygame.image.load(self.settings.bg_file)
         self.bg = pygame.transform.scale(self.bg, 
             (self.settings.screen_w, self.settings.screen_h)
             )
@@ -28,7 +28,7 @@ class AlienInvasion:
         # Game Loop
         while self.running:
             self._check_events()
-
+            self.ship.update()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
 
@@ -43,6 +43,27 @@ class AlienInvasion:
                 self.running = False
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+                self.running = False
+                pygame.quit()
+                sys.exit() 
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -6,8 +6,9 @@ class Settings:
         self.screen_w = 1200
         self.screen_h = 800
         self.FPS = 60
-        self.bg_filee = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
 
-        self.ship_filee = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
+        self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
         self.ship_w = 40
         self.ship_h = 60
+        self.ship_speed = 5
